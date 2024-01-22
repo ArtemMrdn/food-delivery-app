e79a9aab2ea15c0a4fe1b4ee143b2f9188aaf322
ArtemMrdn <artommrdn47@gmail.com>
2024-01-22 12:58:26 +0200

    Added components with styling using SCSS preprocessing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import "./App.css";
+import React from "react";
+import "./scss/app.scss";
+import { Header } from "./components/Header";
+import { Sort } from "./components/Sort";
+import { Categories } from "./components/Categories";
+import { PizzaBlock } from "./components/PizzaBlock";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Мексиканская пицца" price={395} />
+            <PizzaBlock title="Гавайская пицца" price={400} />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
